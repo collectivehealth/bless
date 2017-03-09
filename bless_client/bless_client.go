cd92999abe093ed97324ff8e349c8e5d4aae46ac
Stephanie Sun <stephanie.sun@collectivehealth.com>
2017-03-09 10:16:39 -0800

    Get current user in go

--- bless_client/bless_client.go
+++ bless_client/bless_client.go
@@ -3,6 +3,7 @@ package main
 import (
     "os"
     "log"
+    "os/user"
     "io/ioutil"
     "encoding/json"
     "github.com/aws/aws-sdk-go/aws"
@@ -18,27 +19,26 @@ func check(e error) {
 
 func main() {
     args := os.Args[1:]
-    if len(args) != 9 {
-        log.Fatal(
-            `Usage: bless_client region lambda_function_name bastion_user bastion_user_ip
-             remote_username bastion_ip bastion_command <id_rsa.pub to sign>
-             <output id_rsa-cert.pub>`)
+    if len(args) != 6 {
+        log.Fatal("Usage: bless_client region lambda_function_name bastion_ip bastion_command <id_rsa.pub to sign> <output id_rsa-cert.pub>")
     }
 
-    region, lambda_function_name, bastion_user, bastion_user_ip, remote_username, bastion_ip, bastion_command, public_key_filename, certificate_filename := args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]
+    region, lambda_function_name, bastion_ip, bastion_command, public_key_filename, certificate_filename := args[0], args[1], args[2], args[3], args[4], args[5]
 
-    log.Println(region, lambda_function_name, bastion_user, bastion_user_ip, remote_username, bastion_ip, bastion_command, public_key_filename, certificate_filename)
+    current_user, user_err := user.Current()
+    check(user_err)
 
     public_key, read_err := ioutil.ReadFile(public_key_filename)
     check(read_err)
 
-    payload := map[string]string{"bastion_user": bastion_user, "bastion_user_ip": bastion_user_ip,
-                                 "remote_username": remote_username, "bastion_ip": bastion_ip,
+    payload := map[string]string{"bastion_user": current_user.Username, "bastion_user_ip": bastion_ip,
+                                 "remote_username": current_user.Username, "bastion_ip": bastion_ip,
                                  "command": bastion_command, "public_key_to_sign": string(public_key)}
     payload_json, marshal_err := json.Marshal(payload)
     check(marshal_err)
 
-    log.Println("Executing:")
+    log.Println("Executing in " + region)
+
     sess := session.Must(session.NewSession())
     svc := lambda.New(sess, &aws.Config{
         Region: aws.String(region),
@@ -56,9 +56,6 @@ func main() {
     if int(*resp.StatusCode) != 200 {
         log.Fatal("Error creating cert.")
     }
-
-    cert := string(resp.Payload[:])
-    log.Println(cert)
 
     cert_err := ioutil.WriteFile(certificate_filename, resp.Payload[1:len(resp.Payload) - 3], 0600)
     check(cert_err)
